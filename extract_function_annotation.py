6b1eda7323c5badbabe67b399df599ca02f726a6
fplazaonate <fplazaonate@xstoocky01.mgps.inra.fr>
2017-06-21 16:40:50 +0200

    Add option to keep only the most relevant KO

--- extract_function_annotation.py
+++ extract_function_annotation.py
@@ -45,9 +45,11 @@ def get_parameters():
 	parser.add_argument('-k', '--ko-genes', dest='ko_genes_file', type=is_file, required=True,
 			help='File which associate each KEGG gene to a KO')
 
+	parser.add_argument('-b', '--best-hit', dest='best_hit', action="store_true", default=False,
+			help='Assign gene to the KO corresponding to the best hit.')
+
 	parser.add_argument('-o', '--functionnal-annotation', dest='functionnal_annotation_file', required=True,
 			help='Output file which contains functionnal annotation of query genes')
-
 
 	return parser.parse_args()
 
@@ -59,19 +61,21 @@ def index_kegg_genes(ko_genes_file):
 			kegg_gene_to_ko[gene]=ko[3:]
 	return kegg_gene_to_ko
 
-def extract_functionnal_annotation(kegg_gene_to_ko, hits_file):
+def extract_functionnal_annotation(kegg_gene_to_ko, hits_file, best_hit):
 	query_gene_to_ko=OrderedDefaultDict(set)
 	with open(hits_file, 'r') as istream:
 		for line in istream:
 			query_gene, kegg_gene = line.split()[:2]
-			query_gene_to_ko[query_gene].add(kegg_gene_to_ko[kegg_gene])
+			if not best_hit or query_gene not in query_gene_to_ko:
+				query_gene_to_ko[query_gene].add(kegg_gene_to_ko[kegg_gene])
 	
 	return query_gene_to_ko
 
 def write_functionnal_annotation(query_gene_to_ko, functionnal_annotation_file):
 	with open(functionnal_annotation_file,'w') as ostream:
-		for query_gene, ko_annotations in query_gene_to_ko.items():
-			print('{0}\t{1}'.format(query_gene, ','.join(ko_annotations)), file=ostream)
+		for query_gene in query_gene_to_ko:
+			for ko_annotation in query_gene_to_ko[query_gene]:
+				print('{0}\t{1}'.format(query_gene,ko_annotation), file=ostream)
 
 def main():
 	parameters = get_parameters()
@@ -81,7 +85,7 @@ def main():
 	print('Done. {0} KEGG genes indexed.\n'.format(len(kegg_gene_to_ko)))
 
 	print('Extracting query genes functionnal annotation...')
-	query_gene_to_ko=extract_functionnal_annotation(kegg_gene_to_ko, parameters.hits_file)
+	query_gene_to_ko=extract_functionnal_annotation(kegg_gene_to_ko, parameters.hits_file, parameters.best_hit)
 	print('Done. {0} query genes annotated.\n'.format(len(query_gene_to_ko)))
 
 	print('Write query genes functionnal annotation...')
